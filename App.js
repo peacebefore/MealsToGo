177703b9d038de30e550cdd371e98d9b803ea2a0
peacebefore <42794888+peacebefore@users.noreply.github.com>
2021-04-08 07:37:55 -0700

    installed firebase functions

--- App.js
+++ App.js
@@ -1,6 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/functions";
+
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAEXlTVZnu9a9PYgQTGUdLh_XAgobHA3Dc",
+  authDomain: "mealstogo-pg.firebaseapp.com",
+  projectId: "mealstogo-pg",
+  storageBucket: "mealstogo-pg.appspot.com",
+  messagingSenderId: "971158119643",
+  appId: "1:971158119643:web:1bf6516b5af4e7589258fe",
+  measurementId: "G-GHY3GKH3FC",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export default function App() {
   return (
@@ -14,8 +34,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
